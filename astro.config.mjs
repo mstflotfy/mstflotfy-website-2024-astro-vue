14b5c22dcbb1f56e7f23d4fcc9acfdbcc0c17ac9
mstflotfy <mstf.lotfy@gmail.com>
2024-04-18 14:49:50 +0200

    add radix dep & astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import radixVue from "radix-vue";
+import icon from "astro-icon";
 
 export default defineConfig({
   integrations: [
     vue(),
-    radixVue
+    radixVue,
+    icon({
+      include: {
+        mdi: ["*"], // (Default) Loads entire Material Design Icon set
+        mdi: ["account"], // Loads only Material Design Icon's "account" SVG
+      },
+    }),
   ]
 });
